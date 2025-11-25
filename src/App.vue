3c466b95285ebb7efd2ca5215d27c99c3ed22c3f
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-11-25 07:05:03 +0600

    scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,51 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <!-- warpper  -->
+     <div class="wrapper">
+
+  <!-- Preloader -->
+  <!-- <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="/admin/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+  </div> -->
+
+  <!-- Navbar -->
+  <nav-bar></nav-bar>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <side-bar></side-bar>
+  <!-- Main Sidebar End  -->
+
+  <!-- Content Wrapper. Contains page content -->
+  <router-view></router-view>
+  <!-- /.content-wrapper -->
+  <!-- <footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.1.0
+    </div>
+  </footer> -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
   </nav>
-  <router-view/>
+  <!-- <router-view/> -->
 </template>
+<script>
+  import NavBar from './components/NavBar.vue'
+  import SideBar from './components/SideBar.vue'
+  export default {
+    components: {
+      NavBar,SideBar
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    }
+  }
+</script>
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/pages/Home.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: Home
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
-  }
+  
 ]
 
 const router = createRouter({
